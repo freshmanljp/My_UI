caf125632ea30231cb743e3d21368d908ce101ca
ljp <1498725065@qq.com>
2020-07-03 22:17:24 +0800

    完成按钮组件部分

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <MyButton @click="sayHi">哎呀</MyButton>
+      <MyButton type="primary">哎呀</MyButton>
+      <MyButton type="success">哎呀</MyButton>
+      <MyButton type="warning">哎呀</MyButton>
+      <MyButton type="danger">哎呀</MyButton>
+      <MyButton type="info">哎呀</MyButton>
+    </div>
+    <div>
+      <MyButton :plain="true">哎呀</MyButton>
+      <MyButton type="primary" :plain="true">哎呀</MyButton>
+      <MyButton type="success" :plain="true">哎呀</MyButton>
+      <MyButton type="warning" :plain="true">哎呀</MyButton>
+      <MyButton type="danger" :plain="true">哎呀</MyButton>
+      <MyButton type="info" :plain="true">哎呀</MyButton>
+    </div>
+    <div>
+      <MyButton :plain="true" :round="true">哎呀</MyButton>
+      <MyButton type="primary" :plain="true" :round="true">哎呀</MyButton>
+      <MyButton type="success" :plain="true" :round="true">哎呀</MyButton>
+      <MyButton type="warning" :plain="true" :round="true">哎呀</MyButton>
+      <MyButton type="danger" :plain="true" :round="true">哎呀</MyButton>
+      <MyButton type="info" :plain="true" :round="true">哎呀</MyButton>
+    </div>
+    <div>
+      <MyButton :plain="true" :circle="true" icon="address">哎呀</MyButton>
+      <MyButton type="primary" :plain="true" :circle="true" icon="address">哎呀</MyButton>
+      <MyButton type="success" :plain="true" :circle="true" icon="address">哎呀</MyButton>
+      <MyButton type="warning" :plain="true" :circle="true" icon="address">哎呀</MyButton>
+      <MyButton type="danger" :plain="true" :circle="true" icon="address">哎呀</MyButton>
+      <MyButton type="info" :plain="true" :circle="true" icon="address">哎呀</MyButton>
+    </div>
+    <div>
+      <MyButton icon="address"></MyButton>
+      <MyButton type="primary" icon="close1"></MyButton>
+      <MyButton type="success" icon="cancel1"></MyButton>
+      <MyButton type="warning" icon="entertainmentmark"></MyButton>
+      <MyButton type="danger" icon="address"></MyButton>
+      <MyButton type="info" icon="address"></MyButton>
+    </div>
+    <div>
+      <MyButton @click="sayHi" :disabled="true">哎呀</MyButton>
+      <MyButton type="primary" :disabled="true">哎呀</MyButton>
+      <MyButton type="success" :disabled="true">哎呀</MyButton>
+      <MyButton type="warning" :disabled="true">哎呀</MyButton>
+      <MyButton type="danger" :disabled="true">哎呀</MyButton>
+      <MyButton type="info" :disabled="true">哎呀</MyButton>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    sayHi () {
+      console.log('Hi')
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/fonts/iconfont.css'
+import MyButton from './package/button.vue'
 
 Vue.config.productionTip = false
+// 挂载自己封装的UI组件
+Vue.component(MyButton.name, MyButton)
 
 new Vue({
   render: h => h(App)
